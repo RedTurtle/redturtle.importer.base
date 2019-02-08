bfeb46e5d9fa3a3af16b5c339b2287f18b3f0825
Daniele Andreotti <daniele.andreotti@redturtle.it>
2019-02-08 16:27:02 +0100

    Back to development: 1.0.5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='redturtle.importer.base',
-    version='1.0.4',
+    version='1.0.5.dev0',
     description="Imports contents from a json source",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers

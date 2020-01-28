fc062f1dae993165f7cc09aeaba4bb583299fa3d
Andrea Cecchi <andrea.cecchi85@gmail.com>
2020-01-28 16:01:49 +0100

    fix open file error

--- src/redturtle/importer/base/utils.py
+++ src/redturtle/importer/base/utils.py
@@ -1,30 +1,32 @@
 # -*- coding: utf-8 -*-
 from collective.transmogrifier.transmogrifier import configuration_registry
 
-import six.moves.configparser
+from six.moves.configparser import ConfigParser
 
 
 def get_base_config(section="", all=False):
     base_config_path = configuration_registry.getConfiguration(
         "redturtlePlone5Main"
     ).get("configuration")
-    config = six.moves.configparser.ConfigParser()
-    config.read(base_config_path)
-    if all:
-        return get_all_config(config)
-    if not config.has_section(section):
-        return {}
-    return get_config_for_section(config, section)
+    with open(base_config_path) as fp:
+        config = ConfigParser()
+        config.readfp(fp)
+        if all:
+            return get_all_config(config)
+        if not config.has_section(section):
+            return {}
+        return get_config_for_section(config, section)
 
 
 def get_additional_config(section="", all=False):
-    config = six.moves.configparser.ConfigParser()
-    config.read(".migrationconfig.cfg")
-    if all:
-        return get_all_config(config)
-    if not config.has_section(section):
-        return {}
-    return get_config_for_section(config, section)
+    with open(".migrationconfig.cfg") as fp:
+        config = ConfigParser()
+        config.readfp(fp)
+        if all:
+            return get_all_config(config)
+        if not config.has_section(section):
+            return {}
+        return get_config_for_section(config, section)
 
 
 def get_all_config(config):

b4225d5c7be6295e4b7deeae98a502bbacdda597
Andrea Cecchi <andrea.cecchi85@gmail.com>
2022-04-10 13:41:44 +0200

    Back to development: 2.1.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="redturtle.importer.base",
-    version="2.1.0",
+    version="2.1.1.dev0",
     description="Imports contents from a json source",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
